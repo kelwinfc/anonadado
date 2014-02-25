2226ab913f4b99d3272775d0dce950c2cd07ec17
kelwinfc <kelwinfc@caracas.(none)>
2014-02-25 17:00:18 +0000

    Estructura base del dominio. Se agregaron los atributos tipo: bool, string, int, choice.

--- src/annotations.py
+++ src/annotations.py
@@ -1,64 +1,138 @@
-class annotation:
+import json
 
-    def __init__(self, name):
-        self.start = 0
-        self.end = -1
-        self.name = name
+class feature:
+    def __init__(self):
+        self.name = ""
+        self.default = None
+        self.ftype = None
+        self.value = None
+    
+    def __init__(self, json, default=None):
+        self.name = json.get("name", "")
+        self.default = json.get("default", default)
+        self.ftype = json.get("type", None)
+        self.value = json.get("value", None)
+    
+    def __str__(self):
+        return str(self.to_json())
+
+    def to_json(self):
+        ret = {"name" : self.name,
+               "default" : self.default,
+               "type" : self.ftype
+              }
+        if self.value is not None:
+            ret["value"] = self.value
+        if self.ftype is not None:
+            ret["type"] = self.ftype
+
+        return ret
+    
+    def get_instance(self):
+        return feature(self.to_json())
+
+class bool_feature(feature):
+    def __init__(self, json):
+        feature.__init__(self, json, True)
+    
+    def get_instance(self):
+        return bool_feature(self.to_json())
+
+class str_feature(feature):
+    def __init__(self, json):
+        feature.__init__(self, json, "")
+    
+    def get_instance(self):
+        return str_feature(self.to_json())
+
+class int_feature(feature):
+    def __init__(self, json):
+        feature.__init__(self, json, 0)
+    
+    def get_instance(self):
+        return int_feature(self.to_json())
+
+class choice_feature(feature):
+    def __init__(self, json):
+        feature.__init__(self, json)
+        self.values = []
+        
+        for v in json.get("values",[]):
+            self.values.append(v)
+        self.value = json.get("value", None)
+
+        if not self.value in self.values:
+            self.value = None
+        if not self.default in self.values:
+            self.default = None
+    
+    def to_json(self):
+        ret = feature.to_json(self)
+        ret["values"] = self.values
+        return ret
+
+class_by_name = {"bool": bool_feature,
+                 "string": str_feature,
+                 "int": int_feature,
+                 "choice": choice_feature
+                }
+
+def get_class_by_type(t):
+    return class_by_name.get(t, feature)
+
+class annotation:
+    
+    def __init__(self, json):
+        self.start = json.get("start", -1)
+        self.end = json.get("end", -1)
+        self.name = json.get("name", "")
+        self.is_unique = json.get("is_unique", False)
+        self.is_global = json.get("is_global", False)
+        
+        self.features = []
+        for f in json.get("features",[]):
+            self.features.append(get_class_by_type(f["type"])(f))
+    
+    def __str__(self):
+        return str(self.to_json())
+    
+    def to_json(self):
+        ret = { "name" : self.name,
+                "is_unique" : self.is_unique,
+                "is_global" : self.is_global,
+                "features" : map(lambda x : x.to_json(), self.features)
+              }
+        if self.start != -1:
+            ret["start"] = self.start
+        if self.end != -1:
+            ret["end"] = self.end
+        
+        return ret
+
+    def get_instance(self):
+        return annotation(self.to_json())
     
     def set_interval(self, a, b):
         self.start = a
         self.end = b
 
-    def from_json(json):
-        return annotation("")
-    
-    def draw(self, screen):
-        pass
-
-class bool_annotation(annotation):
-
-    def __init__(self, name):
-        annotation.__init__(self, name)
-        self.value = None
-
-    def from_json(json):
-        return annotation("")
-    
-    def set_value(self,v):
-        self.value = v
-    
-    def draw(self,screen):
-        pass
-
-class point_annotation(annotation):
-
-    def __init__(self, name):
-        pass
-
-class bounding_box_annotation(annotation):
-
-    def __init__(self, name):
-        pass
-
-class polygon_annotation(annotation):
-
-    def __init__(self, name, default=[]):
-        pass
-
-class text_annotation(annotation):
-
-    def __init__(self, name, default=""):
-        pass
-
-class float_annotation(annotation):
-
-    def __init__(self, name, default=0.0):
-        pass
-
 class annotation_manager:
-
+    
     def __init__(self, domain_filename=None, instance_filename=None):
         self.domain = {}
+        self.labels = []
+        self.name = ""
+        
+        if domain_filename is not None:
+            f = open(domain_filename).read()
+            d = json.loads(f)
+            self.name = d["name"]
+            for l in d["labels"]:
+                self.domain[l["name"]] = annotation(l)
+            
+            for l in self.domain:
+                print self.domain[l]
+                print
     
     def add_label(s):
         self.domain[s.name] = s
--- src/anonadado.py
+++ src/anonadado.py
@@ -16,28 +16,34 @@ pygame.init()
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
 
-a = bool_annotation("foo")
+domain_filename = None
+instance_filename = None
 
-am = annotation_manager()
+if len(sys.argv) > 1:
+    domain_filename = sys.argv[1]
+if len(sys.argv) > 2:
+    instance_filename = sys.argv[2]
 
-cap = cv2.VideoCapture('test/0.mpg')
+am = annotation_manager(domain_filename, instance_filename)
 
-counter = 0
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    if not ret:
-        break
-    
-    counter += 1
-    
-    #cv2.imshow('frame', frame)
-    #if cv2.waitKey(1) & 0xFF == ord('q'):
+#cap = cv2.VideoCapture('test/0.mpg')
+
+#counter = 0
+#while(cap.isOpened()):
+    #ret, frame = cap.read()
+    #if not ret:
         #break
     
-    if counter % 100 == 0:
-        print counter, cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
+    #counter += 1
+    
+    ##cv2.imshow('frame', frame)
+    ##if cv2.waitKey(1) & 0xFF == ord('q'):
+        ##break
+    
+    #if counter % 100 == 0:
+        #print counter, cap.get(cv.CV_CAP_PROP_FRAME_COUNT)
 
-print counter
+#print counter
 
-cap.release()
-cv2.destroyAllWindows()
+#cap.release()
+#cv2.destroyAllWindows()
