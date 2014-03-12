0673c21c1f46075ff3315fd04f3610ca472ab56d
kelwinfc <kelwinfc@caracas.(none)>
2014-03-12 13:44:25 +0000

    Refactorizado panel del dominio.

--- src/anonadado_domain.py
+++ src/anonadado_domain.py
@@ -7,81 +7,109 @@ import wx
 
 class DomainPanel(wx.Panel):
 
+    def createControls(self):
+
+        # Add Label Form
+        self.addLabelButton = \
+            wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                            bitmap=wx.Bitmap('media/add.png'))
+        self.addLabelLabel = wx.StaticText(self, label="Label:")
+        self.addLabelInput = wx.TextCtrl(self, value="",
+                                         style=wx.TE_PROCESS_ENTER)
+
+        # List of domain labels
+        self.domainLabelsLabel = wx.StaticText(self, wx.ID_ANY,
+                                               "Domain labels")
+        self.domainLabelsList = wx.ListBox(self, wx.ID_ANY, wx.DefaultPosition,
+                                           (200, 150), [],
+                                           wx.LB_SINGLE|wx.EXPAND)
+        self.domainLabelsList.SetSelection(0)
+        
+        # Global commands (Load, Save, New, ...)
+        self.openDomainButton = wx.BitmapButton(self, id=wx.ID_ANY,
+          bitmap=wx.Bitmap('media/open.png'), style=wx.NO_BORDER, pos=(10, 10))
+        self.saveDomainButton = wx.BitmapButton(self, id=wx.ID_ANY,
+          bitmap=wx.Bitmap('media/save.png'), style=wx.NO_BORDER, pos=(10, 10))
+
+    def addTooltips(self):
+        self.addLabelButton.SetToolTip(wx.ToolTip("Add label to domain"))
+        self.openDomainButton.SetToolTip(wx.ToolTip("Open domain"))
+        self.saveDomainButton.SetToolTip(wx.ToolTip("Save the domain"))
+    
+    def bindControls(self):
+        self.addLabelInput.Bind(wx.EVT_TEXT_ENTER, self.OnAddLabel)
+        self.addLabelButton.Bind(wx.EVT_BUTTON, self.OnAddLabel)
+        self.domainLabelsList.Bind(wx.EVT_LISTBOX, self.OnSelect)
+
+        self.openDomainButton.Bind(wx.EVT_BUTTON, self.top_app.OnLoadDomain)
+        self.saveDomainButton.Bind(wx.EVT_BUTTON, self.top_app.OnSaveDomain)
+    
+    def setLayout(self):
+        def addToSizer(sizer, item):
+            sizer.Add(item, 0, wx.ALL, 5)
+        
+        sizer = wx.BoxSizer(wx.HORIZONTAL)          # Global
+        left_sizer = wx.BoxSizer(wx.VERTICAL)       # Left bar
+        right_sizer = wx.BoxSizer(wx.VERTICAL)      # Right bar
+        form_sizer = wx.BoxSizer(wx.HORIZONTAL)     # Add label form
+        list_sizer = wx.BoxSizer(wx.VERTICAL)       # List of labels
+        commands_sizer = wx.BoxSizer(wx.HORIZONTAL) # Commands
+        
+        seq = [
+               # Skeleton
+               (sizer, left_sizer), (sizer, right_sizer),
+               (left_sizer, form_sizer),
+               (left_sizer, list_sizer),
+               (left_sizer, commands_sizer),
+               
+               # Add Label Form
+               (form_sizer, self.addLabelLabel),
+               (form_sizer, self.addLabelInput),
+               (form_sizer, self.addLabelButton),
+
+               # List of labels
+               (list_sizer, self.domainLabelsLabel),
+               (list_sizer, self.domainLabelsList),
+
+               # Commands
+               (commands_sizer, self.openDomainButton),
+               (commands_sizer, self.saveDomainButton)
+              ]
+        
+        for (s,i) in seq:
+            addToSizer(s, i)
+        
+        self.SetSizer(sizer)
+        self.load_domain()
+    
     def __init__(self, parent, an):
 
         wx.Panel.__init__(self, parent=parent, id=wx.NewId())
         self.top_app = an
-        sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        left_sizer = wx.BoxSizer(wx.VERTICAL)
-        right_sizer = wx.BoxSizer(wx.VERTICAL)
-
-        sizer.Add(left_sizer, 0, wx.ALL, 5)
-        sizer.Add(right_sizer, 0, wx.ALL, 5)
-
-        # Add label form
-        saveButton = wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
-                                          bitmap=wx.Bitmap('media/add.png'))
-        label = wx.StaticText(self, label="Label:")
-        self.labelTextCtrl = wx.TextCtrl(self, value="",
-                                         style=wx.TE_PROCESS_ENTER)
-        self.labelTextCtrl.Bind(wx.EVT_TEXT_ENTER, self.add_label)
         
-        form_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        form_sizer.Add(label, 0, wx.ALL, 5)
-        form_sizer.Add(self.labelTextCtrl, 0, wx.ALL, 5)
-        form_sizer.Add(saveButton, 0, wx.ALL, 5)
-        saveButton.Bind(wx.EVT_BUTTON, self.add_label)
-
-        left_sizer.Add(form_sizer, 0, wx.ALL, 5)
-
-        # List of domain labels
-        self.domain_labels = wx.ListBox(self, wx.ID_ANY, wx.DefaultPosition,
-                                        (200, 150),
-                                        [], wx.LB_SINGLE|wx.EXPAND)
-        self.domain_labels.SetSelection(0)
-        self.domain_labels.Bind(wx.EVT_LISTBOX, self.OnSelect)
-
-        left_sizer.Add(wx.StaticText(self, wx.ID_ANY, "Domain labels"))
-        left_sizer.Add(self.domain_labels, wx.ID_ANY, wx.ALL)
-
-        # Global commands (Load, Save, New)
-        commands_sizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        open_domain = wx.BitmapButton(self, id=wx.ID_ANY,
-                                      bitmap=wx.Bitmap('media/open.png'),
-                                      style=wx.NO_BORDER,
-                                      pos=(10, 10))
-        save_domain = wx.BitmapButton(self, id=wx.ID_ANY,
-                                      bitmap=wx.Bitmap('media/save.png'),
-                                      style=wx.NO_BORDER,
-                                      pos=(10, 10))
-
-        open_domain.Bind(wx.EVT_BUTTON, self.top_app.OnLoadDomain)
-        save_domain.Bind(wx.EVT_BUTTON, self.top_app.OnSaveDomain)
-
-        commands_sizer.Add(open_domain, 0, wx.ALL, 5)
-        commands_sizer.Add(save_domain, 0, wx.ALL, 5)
-
-        left_sizer.Add(commands_sizer, 0, wx.ALL, 5)
-
-        self.SetSizer(sizer)
-        self.load_domain()
-
+        self.createControls()
+        print "created"
+        self.addTooltips()
+        print "tooltips added"
+        self.bindControls()
+        print "controls binded"
+        self.setLayout()
+        print "layout"
+    
     def OnSelect(self, event):
         index = event.GetSelection()
 
         print index
 
-    def add_label(self, event):
-        name = self.labelTextCtrl.GetValue()
+    def OnAddLabel(self, event):
+        name = self.addLabelInput.GetValue()
         if name != "":
             self.top_app.am.domain[name] = annotation({"name":name})
             self.load_domain()
-
+    
     def load_domain(self):
-        self.domain_labels.Set([])
-
+        self.domainLabelsList.Set([])
+        
         if self.top_app.am is not None:
             for k in self.top_app.am.domain.keys():
-                self.domain_labels.Append(k)
+                self.domainLabelsList.Append(k)
