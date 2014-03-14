683b2fde04ed72e923b06516d55b0677cc8878f5
kelwinfc <kelwinfc@caracas.(none)>
2014-03-14 11:52:23 +0000

    Agregadas funciones para cambiar la posición de un feature en las anotaciones.

--- src/anonadado_domain.py
+++ src/anonadado_domain.py
@@ -5,8 +5,8 @@ import json
 from os import getcwd as cwd
 from annotations import *
 import wx
-import wx.lib.scrolledpanel as wxScrolled
-import wx.lib.intctrl as wxIntctrl
+import wx.lib.scrolledpanel as scrolled
+import wx.lib.intctrl as intctrl
 
 class FeatureWidget(wx.Panel):
     def __init__(self, parent, an, annotation, feature, id):
@@ -24,6 +24,13 @@ class FeatureWidget(wx.Panel):
 
     def addTooltips(self):
         pass
+
+    def get_feature_index(self):
+        index = -1
+        for idx, f in enumerate(self.annotation.features):
+            if f.name == self.feature.name:
+                index = idx
+        return index
     
     def createControls(self):
         self.name = wx.StaticText(self,
@@ -31,20 +38,54 @@ class FeatureWidget(wx.Panel):
                   " (" + self.feature.ftype + ")")
         font = wx.Font(12, wx.DECORATIVE, wx.NORMAL, wx.BOLD)
         self.name.SetFont(font)
+
+        self.removeButton = \
+            wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                            bitmap=wx.Bitmap(cwd() + '/media/remove.png'),
+                            pos=(10,10))
+        
+        if len(self.annotation.features) > 0 and self.get_feature_index() > 0:
+            self.moveUpButton = \
+                wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                                bitmap=wx.Bitmap(cwd() + '/media/up.png'),
+                                pos=(10,10))
+        else:
+            self.moveUpButton = \
+                wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                                pos=(10,10))
+
+        if len(self.annotation.features) > 0 and \
+                self.get_feature_index() + 1 < len(self.annotation.features):
+            self.moveDownButton = \
+                wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                                bitmap=wx.Bitmap(cwd() + '/media/down.png'),
+                                pos=(10,10))
+        else:
+            self.moveDownButton = \
+                wx.BitmapButton(self, id=wx.ID_ANY, style=wx.NO_BORDER,
+                                pos=(10,10))
     
     def setInitialValues(self):
         pass
     
     def bindControls(self):
-        pass
+        self.removeButton.Bind(wx.EVT_BUTTON, self.OnRemoveFeature)
+        self.moveUpButton.Bind(wx.EVT_BUTTON, self.OnMoveUpFeature)
+        self.moveDownButton.Bind(wx.EVT_BUTTON, self.OnMoveDownFeature)
     
     def setLayout(self, extra_values=[]):
         def addToSizer(sizer, item, alignment=wx.ALL):
             sizer.Add(item, 0, alignment, 5)
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.nameSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        seq = [(self.sizer, self.name)]
+        seq = [(self.sizer, self.nameSizer),
+               (self.nameSizer, self.name),
+               (self.nameSizer, self.removeButton),
+               (self.nameSizer, self.moveDownButton),
+               (self.nameSizer, self.moveUpButton),
+              ]
         
         for n in seq:
             a = wx.ALL
@@ -55,9 +96,36 @@ class FeatureWidget(wx.Panel):
             addToSizer(s, i, a)
 
         self.SetSizer(self.sizer)
+        
+    def OnRemoveFeature(self, event):
+        index = self.get_feature_index()
+        if index >= 0:
+            del self.annotation.features[index]
+            
+            label = self.top_app.domainTab.domainLabelsList.GetSelection()
+            self.top_app.domainTab.select_label(label)
+    
+    def OnMoveUpFeature(self, event):
+        index = self.get_feature_index()
+        if 0 < index and index < len(self.annotation.features):
+            tmp = self.annotation.features[index - 1]
+            self.annotation.features[index - 1] = \
+                self.annotation.features[index]
+            self.annotation.features[index] = tmp
 
-#TODO: agregar marca como positivo o negativo dependiendo si el default
-#      value es correcto
+            label = self.top_app.domainTab.domainLabelsList.GetSelection()
+            self.top_app.domainTab.select_label(label)
+    
+    def OnMoveDownFeature(self, event):
+        index = self.get_feature_index()
+        if 0 <= index and index + 1 < len(self.annotation.features):
+            tmp = self.annotation.features[index]
+            self.annotation.features[index] = \
+                self.annotation.features[index + 1]
+            self.annotation.features[index + 1] = tmp
+            
+            label = self.top_app.domainTab.domainLabelsList.GetSelection()
+            self.top_app.domainTab.select_label(label)
 
 class DefaultValueFeatureWidget(FeatureWidget):
     def __init__(self, parent, an, annotation, feature, id):
@@ -74,6 +142,7 @@ class DefaultValueFeatureWidget(FeatureWidget):
         self.validValue = wx.StaticBitmap(self, id=wx.ID_ANY)
     
     def bindControls(self):
+        FeatureWidget.bindControls(self)
         self.Bind(wx.EVT_TEXT, self.OnChangeDefault,
                   id=self.defaultInput.GetId())
     
@@ -84,9 +153,15 @@ class DefaultValueFeatureWidget(FeatureWidget):
 
         self.sizer = wx.BoxSizer(wx.VERTICAL)
         self.defaultSizer = wx.BoxSizer(wx.HORIZONTAL)
-
-        seq = [(self.sizer, self.name),
+        self.nameSizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        seq = [(self.sizer, self.nameSizer),
                (self.sizer, self.defaultSizer),
+
+               (self.nameSizer, self.name),
+               (self.nameSizer, self.removeButton),
+               (self.nameSizer, self.moveDownButton),
+               (self.nameSizer, self.moveUpButton),
 
                (self.defaultSizer, self.defaultLabel),
                (self.defaultSizer, self.defaultInput),
@@ -219,6 +294,7 @@ class ChoiceFeatureWidget(DefaultValueFeatureWidget):
                                     #choices=self.choices)
     
     def bindControls(self):
+        DefaultValueFeatureWidget.bindControls(self)
         self.addChoiceButton.Bind(wx.EVT_BUTTON, self.OnAddChoice)
         self.addChoiceInput.Bind(wx.EVT_TEXT_ENTER, self.OnAddChoice)
         self.defaultInput.Bind(wx.EVT_CHOICE, self.OnChangeDefault)
@@ -228,6 +304,7 @@ class ChoiceFeatureWidget(DefaultValueFeatureWidget):
             sizer.Add(item, 0, alignment, 5)
         
         self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.nameSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.subSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.subLeftSizer = wx.BoxSizer(wx.VERTICAL)
         self.subRightSizer = wx.BoxSizer(wx.VERTICAL)
@@ -235,9 +312,14 @@ class ChoiceFeatureWidget(DefaultValueFeatureWidget):
         self.choicesSizer = wx.BoxSizer(wx.HORIZONTAL)
         self.listSizer = wx.BoxSizer(wx.HORIZONTAL)
         
-        seq = [(self.sizer, self.name),
+        seq = [(self.sizer, self.nameSizer),
                (self.sizer, self.subSizer),
                
+               (self.nameSizer, self.name),
+               (self.nameSizer, self.removeButton),
+               (self.nameSizer, self.moveDownButton),
+               (self.nameSizer, self.moveUpButton),
+
                (self.subSizer, self.subLeftSizer),
                (self.subSizer, self.subRightSizer),
                (self.subLeftSizer, self.defaultSizer),
